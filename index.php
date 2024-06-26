a73d664a71948a1239a676d67cf9251a473089fe
Sargon Kabriel <sergegabriel24@gmail.com>
2024-06-26 12:25:53 +0200

    first-commit

--- index.php
+++ index.php
@@ -1,25 +1,28 @@
 <!DOCTYPE html>
 
 <?php if (is_user_logged_in()) { ?>
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en" class="wpadmin-logged-in">
+  <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en" class="wpadmin-logged-in">
 <?php } else { ?>
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+  <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <?php } ?>
 
 <?php include('header.php'); ?>
 
-  <body>
+<body>
 
-    <main>
+  <main>
 
-      <h1>Home Page</h1>
+    <?php if (have_posts()) : while (have_posts()) : the_post();
+        the_content();
+      endwhile;
+    else : ?>
+      <p>Sorry, no posts matched your criteria.</p>
+    <?php endif; ?>
 
-      <p>This content is hard-coded into the `index.php` file in the root of the theme directory.</p>
+  </main>
 
-    </main>
+  <?php include('footer.php'); ?>
 
-    <?php include('footer.php'); ?>
+</body>
 
-  </body>
-
-</html>
+  </html>
